f90fbfcfce14c9fa410a4179f2e175d5fde799b9
hokirn <1621591162@qq.com>
2021-08-25 18:17:08 +0800

    Final Commit

---
+++ app/src/main/java/com/dong/dongweather/HourlyWeather.java
@@ -0,0 +1,14 @@
+package com.dong.dongweather;
+
+import android.graphics.Bitmap;
+
+
+
+public class HourlyWeather {
+
+    public String hourlyTime;
+
+    public Bitmap hourlyImageBit;
+
+    public String hourlyTemperature;
+}
---
+++ app/src/main/java/com/dong/dongweather/http/MyCallBack.java
@@ -0,0 +1,12 @@
+package com.dong.dongweather.http;
+
+import java.io.IOException;
+
+
+
+public interface MyCallBack {
+
+        void onFailure(IOException e);
+
+        void onResponse(String response) throws IOException;
+}
